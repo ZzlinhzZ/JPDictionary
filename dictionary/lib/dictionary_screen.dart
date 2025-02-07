8c34887c687861902142b217b1cf86b9cf17e0a9
ZzlinhzZ <llinh1413@gmail.com>
2025-02-07 21:41:38 +0700

    fix duplicate error when searching

--- dictionary/lib/dictionary_screen.dart
+++ dictionary/lib/dictionary_screen.dart
@@ -36,12 +36,22 @@ class _DictionaryScreenState extends State<DictionaryScreen> {
     final wordList = await dbHelper.getWordList(query);
     final kanjiList = await dbHelper.getKanjiList(query);
 
+    // Lọc ra các từ `written` duy nhất để tránh trùng lặp trong kết quả tìm kiếm
+    Set<String> uniqueWords = {};
+    List<Word> filteredWords = [];
+    for (var word in wordList) {
+      if (!uniqueWords.contains(word.written)) {
+        uniqueWords.add(word.written);
+        filteredWords.add(word);
+      }
+    }
+
     wordList.sort((a, b) => (a.written == query ? 0 : 1).compareTo(b.written == query ? 0 : 1));
     kanjiList.sort((a, b) => (a.kanji == query ? 0 : 1).compareTo(b.kanji == query ? 0 : 1));
 
     setState(() {
       kanjiResults = kanjiList;
-      wordResults = wordList;
+      wordResults = filteredWords; // Sử dụng danh sách đã lọc
       isSearching = true;
     });
   }
@@ -153,110 +163,3 @@ class _DictionaryScreenState extends State<DictionaryScreen> {
     );
   }
 }
-
-// class VocabularyScreen extends StatelessWidget {
-//   final Word? word;
-
-//   VocabularyScreen({this.word});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     if (word == null) {
-//       return Center(child: Text("Không tìm thấy từ vựng."));
-//     }
-
-//     return Card(
-//       margin: EdgeInsets.all(16),
-//       child: Padding(
-//         padding: EdgeInsets.all(16),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           children: [
-//             Text(word!.written, style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-//             SizedBox(height: 8),
-//             Text('Cách đọc: ${word!.pronounced}', style: TextStyle(fontSize: 18)),
-//             SizedBox(height: 8),
-//             Text('Nghĩa: ${word!.glosses}', style: TextStyle(fontSize: 18)),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-// class KanjiScreen extends StatelessWidget {
-//   final List<Kanji> kanjiList;
-
-//   KanjiScreen({required this.kanjiList});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     if (kanjiList.isEmpty) {
-//       return Center(child: Text("Không tìm thấy hán tự."));
-//     }
-
-//     return ListView.builder(
-//       itemCount: kanjiList.length,
-//       itemBuilder: (context, index) {
-//         final kanji = kanjiList[index];
-//         return Card(
-//           margin: EdgeInsets.all(8),
-//           child: ListTile(
-//             title: Text(kanji.kanji, style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-//             subtitle: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Text('Nghĩa: ${kanji.meanings}'),
-//                 Text('Âm Kun: ${kanji.kunReadings}'),
-//                 Text('Âm On: ${kanji.onReadings}'),
-//                 Text('Số nét: ${kanji.strokeCount}'),
-//               ],
-//             ),
-//             trailing: IconButton(
-//               icon: Icon(Icons.bookmark_border),
-//               onPressed: () {
-//                 // Thêm vào Kanji của tôi
-//               },
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-// class MyKanjiScreen extends StatefulWidget {
-//   @override
-//   _MyKanjiScreenState createState() => _MyKanjiScreenState();
-// }
-
-// class _MyKanjiScreenState extends State<MyKanjiScreen> {
-//   List<String> savedKanji = [];
-
-//   void loadSavedKanji() async {
-//     final db = await DatabaseHelper().database;
-//     final res = await db.query('saved_kanji');
-//     setState(() {
-//       savedKanji = res.map((e) => e['kanji'] as String).toList();
-//     });
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     loadSavedKanji();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return ListView.builder(
-//       itemCount: savedKanji.length,
-//       itemBuilder: (context, index) {
-//         return Card(
-//           margin: EdgeInsets.all(8),
-//           child: ListTile(
-//             title: Text(savedKanji[index], style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
