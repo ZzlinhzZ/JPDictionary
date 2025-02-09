fe3e3f0284a6722ab5c1283abf5b1c81f38c8422
ZzlinhzZ <llinh1413@gmail.com>
2025-02-09 21:57:21 +0700

    fix null value error

--- dictionary/lib/models/KanjiModel.dart
+++ dictionary/lib/models/KanjiModel.dart
@@ -1,43 +1,43 @@
 class Kanji {
   final int id;
   final String kanji;
-  final int grade;
+  final int? grade; // Có thể null
   final int strokeCount;
-  final String meanings;
-  final String kunReadings;
-  final String onReadings;
-  final String nameReadings;
-  final int jlpt;
+  final String? meanings; // Có thể null
+  final String? kunReadings; // Có thể null
+  final String? onReadings; // Có thể null
+  final String? nameReadings; // Có thể null
+  final int? jlpt; // Có thể null
   final String unicode;
-  final String heisigEn;
+  final String? heisigEn; // Có thể null
 
   Kanji({
     required this.id,
     required this.kanji,
-    required this.grade,
+    this.grade,
     required this.strokeCount,
-    required this.meanings,
-    required this.kunReadings,
-    required this.onReadings,
-    required this.nameReadings,
-    required this.jlpt,
+    this.meanings,
+    this.kunReadings,
+    this.onReadings,
+    this.nameReadings,
+    this.jlpt,
     required this.unicode,
-    required this.heisigEn,
+    this.heisigEn,
   });
 
   factory Kanji.fromMap(Map<String, dynamic> map) {
     return Kanji(
       id: map['id'],
       kanji: map['kanji'],
-      grade: map['grade'],
+      grade: map['grade'] as int?,
       strokeCount: map['stroke_count'],
-      meanings: map['meanings'],
-      kunReadings: map['kun_readings'],
-      onReadings: map['on_readings'],
-      nameReadings: map['name_readings'],
-      jlpt: map['jlpt'],
+      meanings: map['meanings'] as String?,
+      kunReadings: map['kun_readings'] as String?,
+      onReadings: map['on_readings'] as String?,
+      nameReadings: map['name_readings'] as String?,
+      jlpt: map['jlpt'] as int?,
       unicode: map['unicode'],
-      heisigEn: map['heisig_en'],
+      heisigEn: map['heisig_en'] as String?,
     );
   }
 }
